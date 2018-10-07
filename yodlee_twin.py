22164a070a299c0e94c077cebf75e40ec47837c5
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2018-10-07 10:45:04 -0400

    organization

--- yodlee_twin.py
+++ yodlee_twin.py
@@ -192,8 +192,7 @@ print(len(EXPENSES_CATEGORY_ID))
 
 
 getCobrandToken()
-getUserToken(INFO['users'][0])
-accounts = getAccounts()
+
 '''sample_json = {}
 sample_json_take_2 = json.loads(getTransactionsSummary(accounts[0],'1900','2020','01','12','01','28',2))
 print(sample_json_take_2["transactionSummary"][0]['debitTotal']['amount']) #gives total amount
@@ -201,32 +200,119 @@ expense = sample_json_take_2["transactionSummary"][0]['debitTotal']['amount']
 sum_of_expenses = 0
 print(sum_of_expenses + expense)'''
 j = 0
-for account in accounts:
-    sum_of_expenses = 0
-    for i in EXPENSES_CATEGORY_ID:
-        try:
-            #print("id: " + str(i))
-            expense = 0
-            #print("transaction summary: " + getTransactionsSummary(accounts[0],'1900','2020','01','12','01','28',i))
-            sample_json_take_2 = json.loads(getTransactionsSummary(account,'1900','2020','01','12','01','28',i,"EXPENSE"))
-            expense = sample_json_take_2["transactionSummary"][0]['debitTotal']['amount']
-            sum_of_expenses += expense
-        except:
-            pass #this is because some of the categories there are no expenses so the json is empty
-    print("Account" + str(j) + "Sum of expenses: " + str(sum_of_expenses))
-    sum_of_incomes = 0
-    for i in INCOME_CATEGORY_ID:
+user_index = 0
+for user in INFO['users']:
+    getUserToken(user)
+    accounts = getAccounts()
+    for account in accounts:
+        print(" Now calculating your expenses")
+        sum_of_expenses = 0
+        for i in EXPENSES_CATEGORY_ID:
             try:
                 #print("id: " + str(i))
-                income = 0
+                expense = 0
                 #print("transaction summary: " + getTransactionsSummary(accounts[0],'1900','2020','01','12','01','28',i))
-                sample_json_take_3 = json.loads(getTransactionsSummary(account,'1900','2020','01','12','01','28',i,"INCOME"))
-                income = sample_json_take_3["transactionSummary"][0]['creditTotal']['amount']
-                sum_of_incomes += income
+                sample_json_take_2 = json.loads(getTransactionsSummary(account,'1900','2020','01','12','01','28',i,"EXPENSE"))
+                expense = sample_json_take_2["transactionSummary"][0]['debitTotal']['amount']
+                if (expense > 0):
+                    if(i == 3):
+                        print("Charitable Giving: " + str(expense))
+                    elif(i == 9):
+                        print("Gifts: " + str(expense))
+                    elif(i == 2):
+                        print("Automative/Fuel: " + str(expense))
+                    elif(i == 11):
+                        print("Healthcare/Medical: " +  str(expense))
+                    elif(i == 14):
+                        print("insurance: " + str(expense))
+                    elif(i == 45):
+                        print("Office expenses: " + str(expense))
+                    elif(i == 16):
+                        print("Services/Supplies: " + str(expense))
+                    elif(i == 104):
+                        print("Postage/Shipping: " + str(expense))
+                    elif(i == 37):
+                        print("Taxes: " + str(expense))
+                    elif(i == 19):
+                        print("Other Expenses: " + str(expense))
+                    elif(i == 33):
+                        print("Check/Payment: " + str(expense))
+                    elif(i == 24):
+                        print("Service Charges/ Fees: " + str(expense))
+                    elif(i == 13):
+                        print("Home Improvement: " + str(expense))
+                    elif(i == 43):
+                        print("Electrics/General Merchandise: " + str(expense))
+                    elif(i == 10):
+                        print("Groceries: " + str(expense))
+                    elif(i == 42):
+                        print("Pets/Pet Care: " + str(expense))
+                    elif(i == 18):
+                        print("Mortgage: " + str(expense))
+                    #----------------------------------------
+                    elif(i == 21):
+                        print("Rent: " + str(expense))
+                    elif(i == 17):
+                        print("Loans: " + str(expense))
+                    elif(i == 39):
+                        print("Utilities: " + str(expense))
+                    elif(i == 15):
+                        print("Cable: " + str(expense))
+                    elif(i == 20):
+                        print("Personal/Family: " + str(expense))
+                    elif(i == 25):
+                        print("ATM/Cash Withdraws: " + str(expense))
+                    elif(i == 6):
+                        print("Education: " + str(expense))
+                    elif(i == 108):
+                        print("Subscription Renewals: " + str(expense))
+                    elif(i == 22):
+                        print("Restaurants: " + str(expense))
+                    elif(i == 7):
+                        print("Entertainment/Recreation: " + str(expense))
+                    elif(i == 23):
+                        print("Travel: " + str(expense))
+                sum_of_expenses += expense
+                #print("User: " + str(user) + "Account" + str(j) + "Sum of expenses: " + str(sum_of_expenses))
             except:
                 pass #this is because some of the categories there are no expenses so the json is empty
-    print("Account" + str(j) + "Sum of incomes: " + str(sum_of_incomes))
-    j += 1 #this is just to keep track of accounts
+        print("User: " + str(user_index) + " Account" + str(j) + " Sum of expenses: " + str(sum_of_expenses))
+        print(" Now calculating your incomes")
+        sum_of_incomes = 0
+        for i in INCOME_CATEGORY_ID:
+                try:
+                    #print("id: " + str(i))
+                    income = 0
+                    #print("transaction summary: " + getTransactionsSummary(accounts[0],'1900','2020','01','12','01','28',i))
+                    sample_json_take_3 = json.loads(getTransactionsSummary(account,'1900','2020','01','12','01','28',i,"INCOME"))
+                    income = sample_json_take_3["transactionSummary"][0]['creditTotal']['amount']
+                    if (income > 0):
+                        if(i == 92):
+                            print("Sales/Service Income: " + str(income))
+                        elif(i == 29):
+                            print("Salary/Regular  Income: " + str(income))
+                        elif(i == 30):
+                            print("Investment/Retirement Income: " + str(income))
+                        elif(i == 96):
+                            print("Interest Income: " +  str(income))
+                        elif(i == 32):
+                            print("Other Income: " + str(income))
+                        elif(i == 225):
+                            print("Rewards: " + str(income))
+                        elif(i == 27):
+                            print("Deposits: " + str(income))
+                        elif(i == 114):
+                            print("Expenses Reimbursements: " + str(income))
+                        elif(i == 227):
+                            print("Refunds/Adjustments: " + str(income))
+                    sum_of_incomes += income
+                except:
+                    pass #this is because some of the categories there are no incomes so the json is empty
+        print("User: " + str(user_index) + " Account" + str(j) + " Sum of incomes: " + str(sum_of_incomes))
+        print("User: " + str(user_index) + " Account " + str(j)+" Number of transactions: ",getTransactionsCount(account,'1900','2020','01','12','01','28'))
+        j += 1 #this is just to keep track of accounts'''
+        user_index += 1
+
 
 '''for account in accounts:
         #account,from_year,to_year,from_month,to_month,from_day,to_day)
